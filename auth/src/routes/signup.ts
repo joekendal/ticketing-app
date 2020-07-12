55c677466e13b099ee65c76e022bb535e286915a
joekendal <13680617+joekendal@users.noreply.github.com>
2020-07-12 15:43:38 +0100

    Added async error handler

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -14,7 +14,7 @@ router.post(
       .isLength({ min: 9, max: 32 })
       .withMessage('Password must be between 9 and 32 characters'),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
