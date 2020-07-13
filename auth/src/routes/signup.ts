fddef93b02e7bbb96a1cd2ea10d5d772c405779e
joekendal <13680617+joekendal@users.noreply.github.com>
2020-07-13 12:08:59 +0100

    Added JWT

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
+import jwt from 'jsonwebtoken';
+
 import { User } from '../models/user';
+import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
@@ -12,8 +14,8 @@ router.post(
     body('email').isEmail().withMessage('Email must be valid'),
     body('password')
       .trim()
-      .isLength({ min: 9, max: 32 })
-      .withMessage('Password must be between 9 and 32 characters'),
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters'),
   ],
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
@@ -32,6 +34,20 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      'asdf'
+    );
+
+    // Store it on session object
+    req.session = {
+      jwt: userJwt,
+    };
 
     res.status(201).send(user);
   }
