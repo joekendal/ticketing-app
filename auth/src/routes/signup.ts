67f16dbc8718775de1ca50c2cce6824e66755520
joekendal <13680617+joekendal@users.noreply.github.com>
2020-07-18 02:04:22 +0100

    Added auth tests

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -14,7 +14,7 @@ router.post(
     body('email').isEmail().withMessage('Email must be valid'),
     body('password')
       .trim()
-      .isLength({ min: 4, max: 20 })
+      .isLength({ min: 6, max: 20 })
       .withMessage('Password must be between 4 and 20 characters'),
   ],
   validateRequest,
